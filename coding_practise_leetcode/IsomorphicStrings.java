2533b2b322e8f14eba257813a22571bc848c1093
prashant-pawar-2197 <prashantpawar2197@gmail.com>
2024-04-02 23:26:34 +0530

    205:Isomorphic strings leetcode

---
+++ coding_practise_leetcode/IsomorphicStrings.java
@@ -0,0 +1,13 @@
+public class IsomorphicStrings {
+    public boolean isIsomorphic(String s, String t) {
+        int counter = 1;
+        int slen, tlen;
+        slen = s.length();
+        tlen = t.length();
+        int[] sCharArr = new int[slen];
+        int[] tCharArr = new int[tlen];
+        for (int i = 0; i < slen; i++) {
+            
+        }
+    }
+}
